147056e8f103a962bf4ab0bffa8165e6347ea74b
Chris Sena <chrissena@gmail.com>
2014-01-29 22:12:11 +0000

    Added MVC 5 Web App and Unit Test Project
    Added MVC 5 Web App and Unit Test Project

---
+++ StudiesWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudiesWebApp.MvcApplication" Language="C#" %>
---
+++ StudiesWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudiesWebApp.Startup))]
+namespace StudiesWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
