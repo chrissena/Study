6a5ad0710787087a0ab98993c887179c18020643
Chris Sena <chrissena@gmail.com>
2014-01-29 17:56:34 +0000

    Fixed Association & Spelling

--- StudyClassLibrary/StudyModel.Designer.cs
+++ StudyClassLibrary/StudyModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model '\\server.sena.chrissena.com\users$\Chris\Documents\Visual Studio 2013\Projects\Study\StudyClassLibrary\StudyModel.edmx'. 
+﻿// T4 code generation is enabled for model '\\server.sena.chrissena.com\users$\chris\Documents\Visual Studio 2013\Projects\Study\StudyClassLibrary\StudyModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
